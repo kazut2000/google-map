dcaec92d807422e4bbae6292caced90e130f7988
kazut2000 <miyahara.kazutoshi132@mail.kyutech.jp>
2023-02-27 11:11:57 +0900

    [add] google map 追加

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ 
+class App extends Component {
+  googleGeocoder = null;
+  constructor(props) {
+    super(props);
+    this.state = {
+      locationName: '',
+      center: {
+        lat: 33.6537089,
+        lng: 130.6722930
+      },
+      isShowMarker: false
+    }
+  }
+  changeLocationName(e) {
+    if (e.key === 'Enter') {
+      this.geocode();
+      return;
+    }
+    this.setState({
+      locationName: e.target.value
+    });
+  }
+  geocode() {
+    const geocoder = new window.google.maps.Geocoder();
+    geocoder.geocode({ address: this.state.locationName }, ( results, status ) => {
+      if (status === 'OK') {
+        let center = Object.assign({}, this.state.center);
+        center = {
+          lat: results[0].geometry.location.lat(), 
+          lng: results[0].geometry.location.lng()
+        };
+        this.setState({
+          center,
+          isShowMarker: true
+        });
+      }
+    });
+  }
+  render() {
+    const labelStyle = {
+      margin: '5px',
+      display: 'block'
+    }
+    const containerStyle = {
+      width: '100%',
+      height: '100vh'
+    };
+    
+    return (
+      <div>
+        <header>
+          <h1>Iizuka Map</h1>
+          <label style={labelStyle}>Input LocationName: <input type='text' onChange={(e) => this.changeLocationName(e)} value={this.state.locationName} onKeyPress={(e) => this.changeLocationName(e)}/></label>
+        </header>
+        <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY}>
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={this.state.center}
+            zoom={16}
+          >
+            { this.state.isShowMarker && <Marker position={this.state.center} /> }
+          </GoogleMap>
+        </LoadScript>
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
